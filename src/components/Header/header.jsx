34d43621eb0baa71f874c9eb393ab0a65fc08c09
Flansh Gajbhiye <rickygajbhiye1137@gmail.com>
2024-05-18 01:12:40 +0530

    menu

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
         <>
         <nav className="bg-blue-900 py-2 text-white border-l-white md:px-12 lg:px-20">
             <div className="flex justify-between items-center px-4">
-                <div className="text-xl ">Menu</div>
+                <div className="text-xl md:hidden">Menu</div>
                 <button onClick={toggleMenu} className={`md:hidden text-white transform transition-transform delay-3000 duration-3000 ${menuOpen ? 'rotate-180' : ''}`}>
                     {menuOpen ? <PiCirclesThreeFill className="w-6 h-6" /> : <PiCirclesThree className="w-6 h-6" />}
                 </button>
