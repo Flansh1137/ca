4a433ca8a97e43a04d6d750121502a3a7171c4b7
Flansh Gajbhiye <rickygajbhiye1137@gmail.com>
2024-05-19 14:01:29 +0530

    menu

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+
+    <div>
+      <div className=" bg-slate-400">
+
+
+        <h1 className=' text-sm font-thin pt-4 pl-4 text-white'> ABOUT 
+         <div className='lg:py-4 md:text-6xl lg:text-left font-semibold text-xl'>
+          <p>CA.UK.<span className=' text-orange-600'>ABC</span></p>
+        </div>
+        </h1>
+
+        
+      </div>
+    </div>
   )
 }
 
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,7 +1,7 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaTwitter, FaFacebookF, FaLinkedinIn } from 'react-icons/fa';
-import { PiCirclesThree , PiCirclesThreeFill } from 'react-icons/pi';
+import { PiCirclesThree, PiCirclesThreeFill } from 'react-icons/pi';
 
 const Topper = () => {
     return (
@@ -25,7 +25,7 @@ const Topper = () => {
                 <div className='lg:py-4 md:text-6xl lg:text-left font-semibold text-center text-3xl py-2 '>
                     <p>CA.UK.<span className=' text-orange-600'>ABC</span></p>
                 </div>
-                <div className="flex lg:text-left text-center flex-col justify-center md:pt-4 lg:pt-0 ">
+                <div className=" hidden  md:flex lg:text-left text-center flex-col justify-center md:pt-4 lg:pt-0  ">
                     <div className="flex flex-row-reverse">
                         <p className="pl-2">
                             <span className="font-bold">Free call:</span> (91) 8600390716
@@ -52,28 +52,28 @@ const Navbar = () => {
         setMenuOpen(!menuOpen);
     };
 
-        const closeMenu = () => {
+    const closeMenu = () => {
         setMenuOpen(false);
     };
     return (
         <>
-        <nav className="bg-blue-900 py-2 text-white border-l-white md:px-12 lg:px-20">
-            <div className="flex justify-between items-center px-4">
-                <div className="text-xl md:hidden">Menu</div>
-                <button onClick={toggleMenu} className={`md:hidden text-white transform transition-transform delay-3000 duration-3000 ${menuOpen ? 'rotate-180' : ''}`}>
-                    {menuOpen ? <PiCirclesThreeFill className="w-6 h-6" /> : <PiCirclesThree className="w-6 h-6" />}
-                </button>
-            </div>
-            <ul className={`md:flex space-x-10 ${menuOpen ? 'block text-right pr-4 ' : 'hidden'}`}>
-                <Link to="/" onClick={closeMenu}><li className="text-white-700 hover:text-orange-500 py-1 md:py-0">Home</li></Link>
-                <Link to="/about" onClick={closeMenu}><li className="text-white-700 hover:text-orange-500 py-1 md:py-0">About us</li></Link>
-                <Link to="/services" onClick={closeMenu}><li className="text-white-700 hover:text-orange-500 py-1 md:py-0">Services</li></Link>
-                <Link to="/casestudy" onClick={closeMenu}><li className="text-white-700 hover:text-orange-500 py-1 md:py-0">Case study</li></Link>
-                <Link to="/blog" onClick={closeMenu}><li className="text-white-700 hover:text-orange-500 py-1 md:py-0">Blog</li></Link>
-                <Link to="/contact" onClick={closeMenu}><li className="text-white-700 hover:text-orange-500 py-1 md:py-0">Contact us</li></Link>
-            </ul>
-        </nav>
-    </>
+            <nav className="bg-blue-900 py-2 text-white border-l-white md:px-12 lg:px-20">
+                <div className="flex justify-between items-center px-4">
+                    <div className="text-xl md:hidden">Menu</div>
+                    <button onClick={toggleMenu} className={`md:hidden text-white transform transition-transform delay-3000 duration-3000 ${menuOpen ? 'rotate-180' : ''}`}>
+                        {menuOpen ? <PiCirclesThreeFill className="w-6 h-6" /> : <PiCirclesThree className="w-6 h-6" />}
+                    </button>
+                </div>
+                <ul className={`md:flex space-x-10 ${menuOpen ? 'block text-right pr-4 ' : 'hidden'}`}>
+                    <Link to="/" onClick={closeMenu}><li className="text-white-700 hover:text-orange-500 py-1 md:py-0">Home</li></Link>
+                    <Link to="/about" onClick={closeMenu}><li className="text-white-700 hover:text-orange-500 py-1 md:py-0">About us</li></Link>
+                    <Link to="/services" onClick={closeMenu}><li className="text-white-700 hover:text-orange-500 py-1 md:py-0">Services</li></Link>
+                    <Link to="/casestudy" onClick={closeMenu}><li className="text-white-700 hover:text-orange-500 py-1 md:py-0">Case study</li></Link>
+                    <Link to="/blog" onClick={closeMenu}><li className="text-white-700 hover:text-orange-500 py-1 md:py-0">Blog</li></Link>
+                    <Link to="/contact" onClick={closeMenu}><li className="text-white-700 hover:text-orange-500 py-1 md:py-0">Contact us</li></Link>
+                </ul>
+            </nav>
+        </>
     );
 };
 
