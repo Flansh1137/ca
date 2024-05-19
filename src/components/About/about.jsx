aeb8c01f2fbed29c43d8ee0d0f29e42a2e6ad4c3
Flansh Gajbhiye <rickygajbhiye1137@gmail.com>
2024-05-19 22:58:29 +0530

    started about us page

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
+import '../About/about.css'
+import aboutTop from '../../assets/images/aboutTop.jpg'
 
 const About = () => {
   return (
 
-    <div>
-      <div className=" bg-slate-400">
-
-
-        <h1 className=' text-sm font-thin pt-4 pl-4 text-white'> ABOUT 
-         <div className='lg:py-4 md:text-6xl lg:text-left font-semibold text-xl'>
-          <p>CA.UK.<span className=' text-orange-600'>ABC</span></p>
-        </div>
-        </h1>
-
-        
+    <>
+    <div className='relative '>
+      <img src={aboutTop} alt="" />
+      <div class=" textAboutUs bg-gradient-to-r from-white from-10% via-white via-30% to-blue-900 to-80%">
+        <p class="py-1 px-14 text-xl md:text-4xl font-semibold text-blue-900 md:py-4  w-full">
+          About us
+        </p>
       </div>
-    </div>
+      </div>
+
+    </>
   )
 }
 
