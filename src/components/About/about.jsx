94565498bca0173b7889d4bd7431cd2a440bc5ab
Flansh Gajbhiye <rickygajbhiye1137@gmail.com>
2024-05-20 02:07:37 +0530

    meet our team

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -19,17 +19,30 @@ const About = () => {
 
 
       <div className=' bg-blue-50 p-8 md:px-20  text-lg  lg:px-32'>
-
-        {/* <p className='font-thin text-xl lg:text-2xl'>We are <br /> <span className='font-bold underline underline-offset-4 text-2xl md:text-3xl  lg:text-4xl'>CA.UK.</span><span className='font-bold underline underline-offset-4 text-2xl md:text-3xl  lg:text-4xl text-orange-600'>ABC</span></p> */}
         <p className=' font-bold underline underline-offset-4 text-2xl md:text-3xl  lg:text-4xl' >What We<span className='font-bold underline underline-offset-4 text-2xl md:text-3xl  lg:text-4xl text-orange-600'> DO</span></p>
 
-        <div className='md:grid grid-cols-2 py-4 md:py-10  '>
+        <div className='md:grid grid-cols-2 pt-4 md:pt-10  '>
 
           <img src={whatWeDo} className=' rounded-3xl	h-full ' />
 
-          <p className='pt-2 lg:pt-28 lg:text-xl align-center px-2 md:pl-12 lg:pl-20'>At CA.UK.ABC, we specialize in a wide array of accounting and financial services tailored to meet the diverse needs of our clients. From meticulous auditing and assurance to strategic tax planning and compliance. We are dedicated to empowering your financial success through our expertise, innovation, and unwavering commitment to your goals.</p>
+          <p className='pt-2 lg:pt-28 lg:text-xl align-center px-2 md:pl-12 lg:pl-20'>At <span className='font-bold'>CA.UK.ABC</span>, we specialize in a wide array of accounting and financial services tailored to meet the diverse needs of our clients. From meticulous auditing and assurance to strategic tax planning and compliance. We are dedicated to empowering your financial success through our expertise, innovation, and unwavering commitment to your goals.</p>
 
-          {/* <img src={aboutTeam} className='w-full' alt="" /> */}
+        </div>
+
+        <div>
+        </div>
+      </div>
+      <div className=' bg-blue-50 p-8 md:px-20  text-lg  lg:px-32'>
+        <p className=' font-bold underline underline-offset-4 text-2xl md:text-3xl  lg:text-4xl' >Meet Our<span className='font-bold underline underline-offset-4 text-2xl md:text-3xl  lg:text-4xl text-orange-600'> Team</span></p>
+
+        <div className=' '>
+
+
+          <p className='pt-2 lg:pt-10 lg:text-xl '>At <span className='font-bold'>CA.UK.ABC</span>, our success is driven by our exceptional team of professionals. From our visionary founders to our dedicated client managers, team leads, and accountants, every member of our team brings a wealth of expertise and a commitment to excellence. Together, we work tirelessly to deliver outstanding service and build lasting relationships with our clients.
+          <img src={aboutTeam} className='w-full pt-4 md:pt-10' alt="" />
+
+</p>
+
         </div>
 
         <div>
