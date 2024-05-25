b022ce0a73e4dfe2819cff776a29c1986e677dd7
Flansh Gajbhiye <rickygajbhiye1137@gmail.com>
2024-05-25 19:40:43 +0530

    service

--- src/components/Servicepage/service.jsx
+++ src/components/Servicepage/service.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import topServices from '../../assets/images/topServices.jpg'
+
 
 const service = () => {
   return (
-    <div>service</div>
+    <>
+
+      {/* banner and services text  */}
+      <div className='relative '>
+        <img src={topServices} alt="" className='w-full' />
+        <div class=" textAboutUs bg-gradient-to-r from-white from-10% via-white via-30% to-blue-900 to-80%">
+          <p class="py-1 px-14 text-xl md:text-5xl font-semibold md:font-bold text-blue-800 md:py-4  w-full">
+            Services
+          </p>
+        </div>
+      </div>
+
+    </>
   )
 }
 
